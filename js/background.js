9826a1790e314029bb8fc544757f323f8694aba8
mozjsy <108272222+mozjsy@users.noreply.github.com>
2022-08-14 21:22:22 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -8,7 +8,7 @@ function handleImageLoad() {
 
 function paintImage(imgNumber) {
   const image = new Image();
-  image.src = `../img/${imgNumber + 1}.jpg`;
+  image.src = `img/${imgNumber + 1}.jpg`;
   image.classList.add("bgImage");
   body.prepend(image);
 }
